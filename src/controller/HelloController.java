0939d88e2b7c49edf01d8fa73ce32c9b0a5cbf64
Heytor Víctor <heytorvas@gmail.com>
2020-11-25 17:36:50 -0300

    first commit

---
+++ src/controller/HelloController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class HelloController implements Serializable{
+
+	private static final long serialVersionUID = -6582255345496150562L;
+	
+}
